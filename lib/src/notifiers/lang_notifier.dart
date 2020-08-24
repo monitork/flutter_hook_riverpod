e8e3bffc2ba529e209893d39876e423af5ccbfff
Nguyen Toai <nguyentoai.dev@gmail.com>
2020-08-24 17:49:39 +0700

    [ADD] get language code

--- lib/src/notifiers/lang_notifier.dart
+++ lib/src/notifiers/lang_notifier.dart
@@ -1,23 +1,40 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hook_riverpod_example/core/utils/shared_preferences.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 //final langProvider = StateProvider((ref) => LangNotifier());
 final langChangeProvider = ChangeNotifierProvider((ref) => LangNotifier());
 
 class LangNotifier extends ChangeNotifier {
   Locale _appLocale = Locale('vi');
+  Locale _deviceLocal;
 
   Locale get appLocal => _appLocale ?? Locale("vi");
 
   fetchLocale() async {
+    var defaultLocale = Platform.localeName.split('_');
+    String languageCode = defaultLocale[0];
+    String countryCode = defaultLocale[1];
     var prefs = await SpUtil.getInstance();
-    if (prefs.getString('language_code') == null) {
-      _appLocale = Locale('vi');
-      return Null;
+    if (prefs.getString('language_code') == null && _deviceLocal == null) {
+      _appLocale = Locale('en');
+    } else if (_deviceLocal != null) {
+      _appLocale = _deviceLocal;
     }
     _appLocale = Locale(prefs.getString('language_code'));
-    return Null;
   }
+
+//  Locale deviceLocale(locale, supportedLocales) {
+//    if (locale == null) {
+//      _deviceLocal = Locale(locale.countryCode);
+//      notifyListeners();
+//      return Locale(locale.countryCode);
+//    } else {
+//      return Locale('en');
+//    }
+//  }
 
   void changeLanguage(Locale type) async {
     var prefs = await SpUtil.getInstance();
